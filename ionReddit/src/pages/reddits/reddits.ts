8ce1cb931137d85d36b1682c206d9ae509ed007e
Nayonna Purnell <nayonnapurnell@outlook.com>
2017-05-23 01:43:22 -0400

    added icons and images

--- ionReddit/src/pages/reddits/reddits.ts
+++ ionReddit/src/pages/reddits/reddits.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { RedditService} from '../../app/services/reddit.service';
+import { DetailsPage } from '../details/details';
 
 @Component({
   selector: 'reddits',
@@ -19,6 +20,12 @@ export class RedditsPage {
   getPosts(category, limit){
     this.redditService.getPosts(category, limit).subscribe(response => {
       this.items = response.data.children;
+    });
+  }
+
+  viewItem(item){
+    this.navCtrl.push(DetailsPage, {
+      item:item
     });
   }
 
